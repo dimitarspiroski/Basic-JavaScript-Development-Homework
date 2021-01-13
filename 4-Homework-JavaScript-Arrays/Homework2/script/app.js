289571f671d8491a86c879f76ecb9b05e72b0eb2
dimitarspiroski <75268872+dimitarspiroski@users.noreply.github.com>
2021-01-13 01:07:49 +0100

    .

--- 4-Homework-JavaScript-Arrays/Homework2/script/app.js
+++ 4-Homework-JavaScript-Arrays/Homework2/script/app.js
@@ -1,13 +1,21 @@
-let array = [2, 5, 7, 10, 20];
-
-function sum(array) {
+let numbersArray = [2, 5, 7, 10, 20];
+let numbersArray2 = [10, 25, 'East', 50, 75];
+function sum(numbersArray) {
     let total = 0;
-    for(let i = 0; i < array.length; i++) {
-        total += Number(array[i]);
+    for(let i = 0; i < numbersArray.length; i++) {
+        total += Number(numbersArray[i]);
     }
     return total;
 }
 
+function validateNumber(numbersArray2){
+    for(let num of numbersArray2){
+        if(!Number.isNaN(num)){
+            console.log(`Array #2 is invalid`);
+            return `Array #2 is invalid`;
+        }
+        }
+    }
 
-
-console.log(sum(array));
+console.log(sum(numbersArray));
+validateNumber(numbersArray2);
