7005a02e4002968b5de55510e1b22a5141b68cef
dimitarspiroski <75268872+dimitarspiroski@users.noreply.github.com>
2021-01-19 18:31:36 +0100

    .

---
+++ 5-Homework-Javascript-DOM/Homework2/script/app.js
@@ -0,0 +1,35 @@
+let numbersArrayDiv = document.querySelector("#numbersArray");
+let sumNumbersDiv = document.querySelector("#sumNumbers");
+let mathEquationDiv = document.querySelector("#bonus");
+
+let numbersArray = [6, 25, 4, 5];
+
+function printList(array) {
+  numbersArrayDiv.innerHTML = "";
+  numbersArrayDiv.innerHTML += "<ul>";
+  for (let i = 0; i < array.length; i++) {
+    numbersArrayDiv.innerHTML += `<li>${array[i]}</li>`;
+  }
+  numbersArrayDiv.innerHTML += "</ul>";
+}
+
+function sumNumbers(array) {
+  let sum = array.reduce(function (a, b) {
+    return a + b;
+  }, 0);
+  sumNumbersDiv.innerHTML = "";
+  sumNumbersDiv.innerHTML += `<p>The sum is: ${sum}</p>`;
+  return sum;
+}
+
+function mathEquation(array) {
+    // let arrayString = [];
+    mathEquationDiv.innerHTML = "";
+    mathEquationDiv.innerHTML += "<p>"
+    mathEquationDiv.innerHTML += array.join('+') + `=${sumNumbers(numbersArray)}`;
+    mathEquationDiv.innerHTML += "</p>"
+}
+
+printList(numbersArray);
+sumNumbers(numbersArray);
+mathEquation(numbersArray);
