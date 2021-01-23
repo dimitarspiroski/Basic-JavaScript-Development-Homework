a7a5e3fefabbb4782e81d276f1030e6d5f966304
dimitarspiroski <75268872+dimitarspiroski@users.noreply.github.com>
2021-01-23 23:00:29 +0100

    Homework Sent

--- 7-Homework-Javascript-Objects/Homework1/script/app.js
+++ 7-Homework-Javascript-Objects/Homework1/script/app.js
@@ -1,10 +1,18 @@
-let dog = {
-  name: "Kaia",
-  kind: "Akita Inu",
-  speak: function (speak) {
+function Animal(name, kind, speak) {
+  this.name = function () {
+    name = prompt("Please enter the name of the animal:");
+  };
+  this.kind = function () {
+    kind = prompt("Please enter the type of the animal:");
+  };
+  this.speak = function () {
     speak = prompt();
-    console.log(`The dog says: ${speak}`);
-  },
-};
+    console.log(`${name}, the ${kind} says: ${speak}`);
+  };
+}
 
-dog.speak(prompt);
+let animal = new Animal();
+
+animal.name(prompt);
+animal.kind(prompt);
+animal.speak(prompt);
