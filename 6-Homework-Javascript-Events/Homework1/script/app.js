90540c03a12354b558147c7e56ea05e5868110d8
dimitarspiroski <75268872+dimitarspiroski@users.noreply.github.com>
2021-01-24 03:15:31 +0100

    Homework Sent

---
+++ 6-Homework-Javascript-Events/Homework1/script/app.js
@@ -0,0 +1,35 @@
+// [Selectors]
+let columnNumber = document.querySelector("#column-number");
+let rowNumber = document.querySelector("#row-number");
+let submitBtn = document.querySelector("#submit-btn");
+let table = document.querySelector("#table-container");
+
+// [Functions]
+function createTable(rows, columns) {
+  let tableBody = document.createElement("tbody");
+  for (let r = 1; r <= rows; r++) {
+    let row = document.createElement("tr");
+    for (let c = 1; c <= columns; c++) {
+      let cell = document.createElement("td");
+      let cellText = document.createTextNode(`Row ${r}, Column ${c}`);
+      cell.appendChild(cellText);
+      row.appendChild(cell);
+    }
+    tableBody.appendChild(row);
+  }
+  table.appendChild(tableBody);
+  table.setAttribute("border", "2");
+  cleanUpInputs();
+}
+
+function cleanUpInputs() {
+  rowNumber.value = "";
+  columnNumber.value = "";
+}
+
+// [Event Handlers]
+submitBtn.addEventListener("click", function () {
+  createTable(rowNumber.value, columnNumber.value);
+});
+
+// I wanted to find a way to delete the previous table when generating a new one, because It keeps adding cells to the previous one, but I can't find a solution...
