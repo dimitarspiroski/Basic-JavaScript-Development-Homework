a33e1e3792fa9aa1e99edc6bde43980805d88a32
dimitarspiroski <75268872+dimitarspiroski@users.noreply.github.com>
2021-02-03 19:03:04 +0100

    Refactor

--- 01-02-2021Test/Exercise1/script/app.js
+++ 01-02-2021Test/Exercise1/script/app.js
@@ -2,8 +2,8 @@ let exArray = [0, 2, 5, 4, 6, 8];
 function dashingNum(array) {
   let string = "";
   for (let n = 0; n < array.length; n++) {
-    if (array[n - 1] % 2 === 0 && array[n] % 2 === 0) {
-      string += `-` + array[n];
+    if (array[n] % 2 === 0 && array[n + 1] % 2 === 0) {
+      string += array[n] + `-`;
     } else {
       string += array[n];
     }
